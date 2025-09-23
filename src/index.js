e8407af469a8e2bfac60214383932fa3e8fd1d1f
ayoubMO19 <ayoubweb04@gmail.com>
2025-09-23 15:19:09 +0200

    cors has been installed

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import path from "path";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
